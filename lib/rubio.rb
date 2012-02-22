70a5a688b16b0e74602a72a1fab7f35a18dfacb4
Robert Ross <robert@maintainedauto.com>
2012-02-21 23:55:08 -0800

    Add some configuration code.

--- lib/rubio.rb
+++ lib/rubio.rb
@@ -1,12 +1,15 @@
 require "rubio/version"
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/core_ext/class/attribute'
-require 'rubio/configuration'
 
 module Rubio
-  extend self
-
-  def configure(&block)
-
+  class << self
+    def configure(&block)
+      Rubio::Configuration.configure(&block)
+    end
   end
+
+  autoload :Configuration, 'rubio/configuration'
 end
+
+Rubio::Configuration.create_options
--- lib/rubio/configuration.rb
+++ lib/rubio/configuration.rb
@@ -1,7 +1,22 @@
 module Rubio
   class Configuration
-    def self.configure(&block)
+    OPTIONS = [:rdio_key, :rdio_secret, :timeout]
 
+    class << self
+      def create_options
+        OPTIONS.each do |option|
+          self.instance_eval <<-RUBY, __FILE__, __LINE__+1
+            def #{option.to_s}(value=nil)
+              @#{option.to_s} = value if value
+              @#{option.to_s}
+            end
+          RUBY
+        end
+      end
+
+      def configure(&block)
+        block.arity < 1 ? instance_eval(&block) : block.call(self)
+      end
     end
   end
 end
