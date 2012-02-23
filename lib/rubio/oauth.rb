a2b8aa3ca323da286f0b7473abfc6f40654c583b
Robert Ross <robert@maintainedauto.com>
2012-02-23 05:16:57 -0800

    Roll own OAuth mechanism that refuses to work.

--- lib/rubio/oauth.rb
+++ lib/rubio/oauth.rb
@@ -1,40 +1,84 @@
-require 'oauth'
 require 'httparty'
+require 'securerandom'
+require 'base64'
+# require 'openssl'
+require 'digest'
+require 'digest/sha1'
 
 module Rubio
   class OAuth
-    API_URL = 'http://api.rdio.com'
+    API_URL = 'http://api.rdio.com/1/'
 
-    class << self
-      def consumer
-        @consumer ||= ::OAuth::Consumer.new(Configuration.rdio_key , Configuration.rdio_secret, {
-          :site               => API_URL,
-          :request_token_path => "/oauth/request_token",
-          :authorize_path     => "/oauth/authorize",
-          :access_token_path  => "/oauth/access_token"
-        })
-      end
+    attr_accessor :access_token, :access_token_secret
 
-      def request_token
-        @request_token ||= self.consumer.get_request_token
-      end
+    def call(method, params)
+      @method = method
+      @params = params
 
-      def access_token_no_auth
-        @access_token ||= ::OAuth::AccessToken.new consumer
-      end
+      response = HTTParty.post(API_URL, :body => {:method => @method}.merge(@params), :headers => headers)
+      response
+    end
 
-      def access_token_auth_url
-        'https://www.rdio.com/oauth/authorize?oauth_token=' + self.request_token.token.to_s
-      end
+    def body
+      URI.escape(@params.collect{|k,v| "#{k}=#{v}"}.join('&'))
+    end
 
-      def access_token
-        @access_token || access_token_no_auth
-      end
+    def headers
+      {'authorization' => authorization_header}
+    end
 
-      def authorized?(value=nil)
-        return @authorized if value.nil?
-        @authorized = !!value
-      end
+    def authorization(values={})
+      values[:oauth_consumer_key] ||= Configuration.rdio_key
+
+      # Uniqueness identifiers
+      values[:oauth_nonce]            = SecureRandom.hex(8)
+      values[:oauth_signature_method] = 'HMAC-SHA1'
+      values[:oauth_timestamp]        = Time.now.to_i.to_s
+
+      values[:oauth_version] = '1.0'
+
+      values
+    end
+
+    def authorization_header
+      values = authorization
+      ap [hmac_key, hmac_body]
+
+      hmac = Digest::HMAC.new(hmac_key, Digest::SHA1)
+      hmac.update(hmac_body)
+
+      values[:oauth_signature] = [hmac.digest].pack('m0').strip
+      ap values[:oauth_signature]
+      
+      'OAuth ' + values.collect {|key,value| "#{key}=\"#{value}\"" }.join(', ')
+    end
+
+    private
+
+    def percent_escape(value)
+      value.bytes.map do |b|
+        c = b.chr
+        if ((c >= '0' and c <= '9') or
+            (c >= 'A' and c <= 'Z') or
+            (c >= 'a' and c <= 'z') or
+            c == '-' or c == '.' or c == '_' or c == '~')
+          c
+        else
+          '%%%02X' % b
+        end
+      end.join
+    end
+
+    def hmac_key
+      parts = [Configuration.rdio_secret, self.access_token_secret]
+      parts.join('&').encode(Encoding::UTF_8)
+    end
+
+    def hmac_body
+      # method, url, body (params)
+      parts = ['POST', API_URL].map {|p| percent_escape(p) }
+      parts << percent_escape({:method => @method}.merge(@params).merge(authorization).to_param)
+      parts.join('&').encode(Encoding::UTF_8)
     end
   end
 end
