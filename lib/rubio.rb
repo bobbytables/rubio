6a353d9ed54a3eed7b4faf0c5680de02baa50f59
Robert Ross <robert@maintainedauto.com>
2012-02-23 05:16:22 -0800

    Add some doc, extend ruby objects.

--- lib/rubio.rb
+++ lib/rubio.rb
@@ -1,9 +1,18 @@
 require "rubio/version"
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/core_ext/class/attribute'
+require 'active_support/core_ext/object'
+require 'ap'
 
 module Rubio
   class << self
+    # Usage:
+    #
+    # Rubio.configure do |c|
+    #   c.rdio_key 'CONSUMER_KEY'
+    #   c.rdio_secret 'CONSUMER_SECRET'
+    # end
+
     def configure(&block)
       Rubio::Configuration.configure(&block)
     end
